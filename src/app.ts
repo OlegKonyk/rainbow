57bfdb76b3440b7e867b2663263a71b26a369c95
Oleg Konyk <okonyk@apple.com>
2017-04-25 07:29:45 -0700

    final for 'From index.html to TypeScript'

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+
 function generateColor() {
     return "#"+((1<<24)*Math.random()|0).toString(16);
 }
@@ -5,6 +6,6 @@ function generateColor() {
 function main() {
     var appComponent = document.getElementById('rainbow')
     setInterval(function() {
-        appComponent.style['border-color'] = generateColor();
-    }, 500)
+        appComponent.style['background-color'] = generateColor();
+    }, 1500)
 }

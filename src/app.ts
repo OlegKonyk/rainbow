6aca962f431803d2aff8fd86f2ac6a8ae4c80085
Oleg Konyk <okonyk@apple.com>
2017-05-09 08:32:30 -0700

    Null and Undefined types

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ function generateColor(): string {
     return "#"+((1<<24)*Math.random()|0).toString(16);
 }
 
-function mutateElement(elId: string, style: ValidStyleName, int: number): void {
+function mutateElement(elId: string, style: ValidStyleName, int?: number): void {
     var appComponent = document.getElementById(elId)
     setInterval(function() {
         var color: string = generateColor();
-        appComponent.style[style] = color;
-    }, int)
+        if (appComponent) {
+            appComponent.style[style] = color;
+        }
+    }, int);
 }
 
 function main() {
